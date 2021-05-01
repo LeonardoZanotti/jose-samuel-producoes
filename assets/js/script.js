fbcf1a4b2998c50abb9b31029be94de1e305f8bc
LeonardoZanotti <leonardojosezanotti@gmail.com>
2021-05-01 18:22:25 -0300

    finished brands carousel and all site i guess

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,44 +19,19 @@ function handleButtonSwitch(button) {
     }
 }
 
-
-
-$(document).ready(function(){
-
-    if($('.brands_slider').length)
-    {
-    var brandsSlider = $('.brands_slider');
-    
-    brandsSlider.owlCarousel(
-    {
-    loop:true,
-    autoplay:true,
-    autoplayTimeout:5000,
-    nav:false,
-    dots:false,
-    autoWidth:true,
-    items:8,
-    margin:42
-    });
-    
-    if($('.brands_prev').length)
-    {
-    var prev = $('.brands_prev');
-    prev.on('click', function()
-    {
-    brandsSlider.trigger('prev.owl.carousel');
-    });
+// brands carousel
+$(document).ready(function() {
+    if($('.brands_slider').length) {
+        var brandsSlider = $('.brands_slider');
+        brandsSlider.owlCarousel({
+            loop:true,
+            autoplay:true,
+            autoplayTimeout:5000,
+            nav:false,
+            dots:false,
+            autoWidth:true,
+            items:8,
+            margin:42
+        });
     }
-    
-    if($('.brands_next').length)
-    {
-    var next = $('.brands_next');
-    next.on('click', function()
-    {
-    brandsSlider.trigger('next.owl.carousel');
-    });
-    }
-    }
-    
-    
-    });
+});
